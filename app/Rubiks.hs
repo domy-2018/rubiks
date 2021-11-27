2e7f616099f77ab8e09a80f47ff6e2ff12dd5b4d
Domy <domy2018developer@protonmail.com>
2021-11-27 23:26:00 +1100

    prototype

--- app/Rubiks.hs
+++ app/Rubiks.hs
@@ -1,7 +1,42 @@
-module Main where
 
 import Cube
-import Control.Monad.RWS
+import Control.Monad.RWS.Strict
+
+-- CubeGame is a type synonym to RWS where
+-- Reader - [String] is the list of moves to be made
+-- Writer - [Cube] list of Cube state transitions for each move
+-- State  - Cube current state of the cube
+-- result - String is the result after executing the moves
+type CubeGame = RWS [String] [Cube] Cube
+
+-- taking the RWS, executes the moves and presents the result to the user
+startGame :: CubeGame String
+startGame = do
+    userinput <- ask
+    return $ "Performing the following moves: " ++ show userinput
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runMain
+  where
+    -- main recursion function
+    runMain = do
+        putStrLn "Make a move: F R B etc.., q to quit"
+        input <- getLine
+        checkInput input
+
+    -- does some sanity check on User interactive input. 
+    -- can be improved to do some regex to ensure valid Cube moves
+    checkInput :: String -> IO ()
+    checkInput a
+        | a /= "q"  = runGame a
+        | otherwise = putStrLn "Thanks for playing"
+
+    -- taking user interactive input, runs the game and executes the moves
+    runGame :: String -> IO ()  
+    runGame input = do
+        let inputList = words input 
+            (a,s,w)   = runRWS startGame inputList initCube
+        print a
+        print s
+        runMain
--- src/Cube.hs
+++ src/Cube.hs
@@ -2,11 +2,13 @@
 
 module Cube where
 
+
+import           Prelude         hiding (Left, Right)
 import qualified Data.Map.Strict as M
 
 
 data Centre = Front | Up | Left | Right | Back | Down
-    deriving Show
+    deriving (Show, Ord, Eq)
 
 data Colour = White | Yellow | Red | Orange | Blue | Green
     deriving Show
@@ -15,4 +17,7 @@ newtype Cube = Cube { centres :: M.Map Centre Colour }
     deriving Show
 
 
-
+initCube :: Cube
+initCube = Cube {
+               centres = M.fromList [(Front, White), (Back, Yellow), (Left, Green), (Right, Blue), (Down, Red)]
+           }
