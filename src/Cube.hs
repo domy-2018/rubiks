2874819519a4068fc98e3d841fd7ef9791667e73
Domy <domy2018developer@protonmail.com>
2021-12-02 19:10:33 +1100

    updated Cube logic to use tuples instead.
    completed rotateCube logic

--- src/Cube.hs
+++ src/Cube.hs
@@ -4,56 +4,126 @@ module Cube where
 
 
 import           Prelude         hiding (Left, Right)
---import qualified Data.Map.Strict as M
-import           Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as IntMap
 import           Moves
 
 
 data Colour = White | Yellow | Red | Orange | Blue | Green
-    deriving Show
+
+instance Show Colour where
+    show White  = "W"
+    show Yellow = "Y"
+    show Red    = "R"
+    show Orange = "O"
+    show Blue   = "B"
+    show Green  = "G"
 
 -- A 2x2 Rubiks cube consists of 8 corners.
--- This is represented by an IntMap from 0-7 with a triple of colours
--- 0 -> Front Left  Up
--- 1 -> Front Right Up
--- 2 -> Front Left  Down
--- 3 -> Front Right Down
--- 4 -> Back  Left  Up
--- 5 -> Back  Right Up
--- 6 -> Back  Left  Down
--- 7 -> Back  Right Down
-newtype Cube = Cube { corners :: IntMap (Colour, Colour, Colour) }
+-- This is represented by a record syntax of 8 triples of colours
+-- flu -> Front Left  Up
+-- fru -> Front Right Up
+-- fld -> Front Left  Down
+-- frd -> Front Right Down
+-- blu -> Back  Left  Up
+-- bru -> Back  Right Up
+-- bld -> Back  Left  Down
+-- brd -> Back  Right Down
+data Cube = Cube { flu :: (Colour, Colour, Colour)
+                 , fru :: (Colour, Colour, Colour)
+                 , fld :: (Colour, Colour, Colour)
+                 , frd :: (Colour, Colour, Colour)
+                 , blu :: (Colour, Colour, Colour)
+                 , bru :: (Colour, Colour, Colour)
+                 , bld :: (Colour, Colour, Colour)
+                 , brd :: (Colour, Colour, Colour) }
     deriving Show
 
 -- initialize the cube
 initCube :: Cube
-initCube = Cube {
-               corners = IntMap.fromList [(0, (White,  Blue,  Red)), 
-                                          (1, (White,  Green, Red)),
-                                          (2, (White,  Blue,  Orange)),
-                                          (3, (White,  Green, Orange)),
-                                          (4, (Yellow, Blue,  Red)),
-                                          (5, (Yellow, Green, Red)),
-                                          (6, (Yellow, Blue,  Orange)),
-                                          (7, (Yellow, Green, Orange))]
-           }
+initCube = Cube { flu = (White,  Blue,  Red)
+                , fru = (White,  Green, Red)
+                , fld = (White,  Blue,  Orange)
+                , frd = (White,  Green, Orange)
+                , blu = (Yellow, Blue,  Red)
+                , bru = (Yellow, Green, Red)
+                , bld = (Yellow, Blue,  Orange)
+                , brd = (Yellow, Green, Orange) }
 
 -- rotates Cube given move
 rotateCube :: Move -> Cube -> Cube
-rotateCube F c = Cube $ 
-    IntMap.adjust (const (c1F, c1U, c1R)) 3 $
-    IntMap.adjust (const (c3F, c3D, c3R)) 2 $
-    IntMap.adjust (const (c0F, c0U, c0L)) 1 $
-    IntMap.adjust (const (c2F, c2D, c2L)) 0 cornerMap
-  where
-    cornerMap :: IntMap (Colour, Colour, Colour)
-    cornerMap = corners c
+rotateCube F c@Cube { flu = (f_flu, l_flu, u_flu)
+                    , fru = (f_fru, r_fru, u_fru)
+                    , fld = (f_fld, l_fld, d_fld)
+                    , frd = (f_frd, r_frd, d_frd) } = c { flu = (f_fld, d_fld, l_fld)
+                                                        , fru = (f_flu, u_flu, l_flu)
+                                                        , fld = (f_frd, d_frd, r_frd)
+                                                        , frd = (f_fru, u_fru, r_fru) }
 
-    (c0F, c0L, c0U) = cornerMap IntMap.! 0
-    (c1F, c1R, c1U) = cornerMap IntMap.! 1
-    (c2F, c2L, c2D) = cornerMap IntMap.! 2
-    (c3F, c3R, c3D) = cornerMap IntMap.! 3
+rotateCube U c@Cube { flu = (f_flu, l_flu, u_flu)
+                    , fru = (f_fru, r_fru, u_fru)
+                    , blu = (b_blu, l_blu, u_blu)
+                    , bru = (b_bru, r_bru, u_bru) } = c { flu = (r_fru, f_fru, u_fru)
+                                                        , fru = (r_bru, b_bru, u_bru)
+                                                        , blu = (l_flu, f_flu, u_flu)
+                                                        , bru = (l_blu, b_blu, u_blu) }
+
+rotateCube L c@Cube { flu = (f_flu, l_flu, u_flu)
+                    , fld = (f_fld, l_fld, d_fld)
+                    , blu = (b_blu, l_blu, u_blu)
+                    , bld = (b_bld, l_bld, d_bld) } = c { flu = (u_blu, l_blu, b_blu)
+                                                        , fld = (u_flu, l_flu, f_flu)
+                                                        , blu = (d_bld, l_bld, b_bld)
+                                                        , bld = (d_fld, l_fld, f_fld) }
+
+rotateCube R c@Cube { fru = (f_fru, r_fru, u_fru)
+                    , frd = (f_frd, r_frd, d_frd)
+                    , bru = (b_bru, r_bru, u_bru)
+                    , brd = (b_brd, r_brd, d_brd) } = c { fru = (d_frd, r_frd, f_frd)
+                                                        , frd = (d_brd, r_brd, b_brd)
+                                                        , bru = (u_fru, r_fru, f_fru)
+                                                        , brd = (u_bru, r_bru, b_bru) }
+
+rotateCube B c@Cube { blu = (b_blu, l_blu, u_blu)
+                    , bru = (b_bru, r_bru, u_bru)
+                    , bld = (b_bld, l_bld, d_bld)
+                    , brd = (b_brd, r_brd, d_brd) } = c { blu = (b_bru, u_bru, r_bru)
+                                                        , bru = (b_brd, d_brd, r_brd)
+                                                        , bld = (b_blu, u_blu, l_blu)
+                                                        , brd = (b_bld, d_bld, l_bld) }
+
+rotateCube D c@Cube { fld = (f_fld, l_fld, d_fld)
+                    , frd = (f_frd, r_frd, d_frd)
+                    , bld = (b_bld, l_bld, d_bld)
+                    , brd = (b_brd, r_brd, d_brd) } = c { fld = (l_bld, b_bld, d_bld)
+                                                        , frd = (l_fld, f_fld, d_fld)
+                                                        , bld = (r_brd, b_brd, d_brd)
+                                                        , brd = (r_frd, f_frd, d_frd) }
+
+rotateCube F' c = rotateCube F $ rotateCube F $ rotateCube F c
+
+rotateCube U' c = rotateCube U $ rotateCube U $ rotateCube U c
+
+rotateCube L' c = rotateCube L $ rotateCube L $ rotateCube L c
+
+rotateCube R' c = rotateCube R $ rotateCube R $ rotateCube R c
+
+rotateCube B' c = rotateCube B $ rotateCube B $ rotateCube B c
+
+rotateCube D' c = rotateCube D $ rotateCube D $ rotateCube D c
+
+rotateCube F2 c = rotateCube F $ rotateCube F c
+
+rotateCube U2 c = rotateCube U $ rotateCube U c
+
+rotateCube L2 c = rotateCube L $ rotateCube L c
+
+rotateCube R2 c = rotateCube R $ rotateCube R c
+
+rotateCube B2 c = rotateCube B $ rotateCube B c
+
+rotateCube D2 c = rotateCube D $ rotateCube D c
 
 
-rotateCube _ _ = undefined
+
+
+
+
